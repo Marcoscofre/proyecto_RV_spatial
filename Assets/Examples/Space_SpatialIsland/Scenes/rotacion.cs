36e7c1ccc2db276e9af3ab27fc05f70fdb3c8206
lasthor <fmanzor.09@gmail.com>
2025-04-25 10:46:06 -0400

    avances 2 escenario direxlab

---
+++ Assets/Examples/Space_SpatialIsland/Scenes/rotacion.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class rotacion : MonoBehaviour
+{
+    public Vector3 velocidadRotacion = new Vector3(0, 0, 50); // Ejes en los que rota (X, Y, Z)
+
+    void Update()
+    {
+        transform.Rotate(velocidadRotacion * Time.deltaTime);
+    }
+}
